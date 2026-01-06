420082d6aa4554b3a5843d25529ac195bedfa8a5
George N. Gouvouniotis <ggouvounotis@proton.me>
2026-01-06 13:30:03 -0500

    Attended issue in index.cshtml. Now user can access ID link and be taken to individual Product.

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections.Generic;
 using Testing.Models;
+
 namespace Testing
 {
     public interface IProductRepository
     {
         IEnumerable<Product> GetAllProducts();
         Product GetProduct(int id);
+        void UpdateProduct(Product product);
     }
 
 }
